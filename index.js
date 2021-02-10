d796cacf9af7b00f113ba071810b1befbea23ea8
Erik Zhao <erikzhao98@gmail.com>
2021-02-10 19:10:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const driver = {
+    'Cash rules everything around me.': 'Wu',
+    'C.R.E.A.M.': 'Tang',
+    'Get the money.': 'For',
+    "$ $ bill, y'all!": 'Ever'
+  };
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+    return Object.assign({}, obj, {[key]: value});
+
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromDriverByKey(obj, key) {
+    let object = Object.assign({}, obj);
+    delete(object[key]);
+    return object;
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+    delete(obj[key]);
+    return obj
+}
